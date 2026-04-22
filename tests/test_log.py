dbf21aa307d7fff8def3fbb2a424ef5785f7370e
Nicolas Girard <ngirard@quantmetry.com>
2026-04-22 11:58:14 +0200

    feat: add loguru as centralized logger (log.py)

---
+++ tests/test_log.py
@@ -0,0 +1,36 @@
+import sys
+import io
+import pytest
+from loguru import logger as loguru_logger
+
+
+def test_logger_importable():
+    from log import logger
+    assert logger is not None
+
+
+def test_logger_emits_debug_to_stderr(capsys):
+    from log import logger
+    buf = io.StringIO()
+    handler_id = logger.add(buf, level="DEBUG", format="{message}")
+    logger.debug("mimir-test-debug-message")
+    logger.remove(handler_id)
+    assert "mimir-test-debug-message" in buf.getvalue()
+
+
+def test_logger_emits_info_to_stderr(capsys):
+    from log import logger
+    buf = io.StringIO()
+    handler_id = logger.add(buf, level="DEBUG", format="{message}")
+    logger.info("mimir-test-info-message")
+    logger.remove(handler_id)
+    assert "mimir-test-info-message" in buf.getvalue()
+
+
+def test_logger_emits_error_to_stderr(capsys):
+    from log import logger
+    buf = io.StringIO()
+    handler_id = logger.add(buf, level="DEBUG", format="{message}")
+    logger.error("mimir-test-error-message")
+    logger.remove(handler_id)
+    assert "mimir-test-error-message" in buf.getvalue()
